7e229374a7f70b20ccb5e9766026aef61297b157
sounddevtechofficial <soundofficialmailbox@gmail.com>
2022-12-24 11:10:20 +0530

    First release of app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'kgcabledashboad',
+    appId: '1:675795898765:web:584f5472f16785c8c52dc4',
+    storageBucket: 'kgcabledashboad.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyABHcHVz-rnnBXTdzZreFNLxECzaVe9yE0',
+    authDomain: 'kgcabledashboad.firebaseapp.com',
+    messagingSenderId: '675795898765',
+    measurementId: 'G-84YCTK7K0F',
+  },
+  production: true
+};
